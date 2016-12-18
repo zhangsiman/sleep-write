f8b37032f186740854dd2d6bf13eb696defd35aa
zhangsiman <1009687043@qq.com>
2016-12-18 13:33:32 +0800

    post

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express= require ('express');
 const app= express();
 const mongoose =require('mongoose')
+const bodyParser=require('body-parser')
 
-
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/digicity')
@@ -17,6 +19,9 @@ app.get('/',function(req,res){
   res.send('hello word')
 })
 
+app.post('/posts',function(req,res){
+  console.log(req.body)
+})
 
 app.listen(4000,function(){
   console.log('ccc');
