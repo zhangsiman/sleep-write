b3a04c7424cdbf334efb968596402706a2da6990
zhangsiman <1009687043@qq.com>
2016-12-18 09:27:12 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+
+class App extends React.Component {
+  render () {
+    return(
+      <div>
+        App
+      </div>
+    )
+
+  }
+}
+
+export default App;
